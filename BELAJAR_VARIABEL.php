58c41c9a588957ddd326a44d482aa3247d3574c4
Haider Yonis <79441745+Haiderstekom123@users.noreply.github.com>
2021-03-03 20:05:35 -0800

    Add files via upload

---
+++ BELAJAR_VARIABEL.php
@@ -0,0 +1,19 @@
+<?php
+
+	echo"=>Memperkenalkan diri<=<br><br>";
+	
+	// Memperkenalkan diri dengan VARIABEL
+	
+	$namalengkap ="Haider Yonis";
+	$alamat ="Pokapaang";
+	$tanggallahir ="20-07-1999";
+	$agama ="Kristen";
+	$status="Mahasiswa";
+	
+	echo "Nama Lengkap : $namalengkap<br>";
+	echo "Alamat		  : $alamat<br>";
+	echo "Tanggal Lahir: $tanggallahir<br>";
+	echo "Agama		  : $agama<br>";
+	echo "Status		  : $status<br>";
+
+?>
